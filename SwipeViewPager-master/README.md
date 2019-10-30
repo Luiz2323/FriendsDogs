7d3b7d9fc600ce8279ba7bcab82c56dbd0b8a94e
carolina <carolinamolano23@gmail.com>
2019-10-30 18:40:45 -0500

    primer commit

---
+++ SwipeViewPager-master/README.md
@@ -0,0 +1,16 @@
+# SwipeViewPager
+
+Card Swipe View using ViewPager
+
+Examples of implementation swiping CardView using ViewPager
+
+## Screenshot
+![Alt text](https://github.com/haerulmuttaqin/SwipeViewPager/blob/master/app/banner.png?raw=true "SwipeViewPager")
+
+
+## Source image from:
+
++ https://www.iamxk.com/71090.html
++ http://influxdesigns.us
++ http://www.zulmaaguiar.com
++ https://armart.info
---
+++ SwipeViewPager-master/app/src/main/java/com/haerul/swipeviewpager/MainActivity.java
@@ -0,0 +1,77 @@
+package com.haerul.swipeviewpager;
+
+import android.animation.ArgbEvaluator;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        models = new ArrayList<>();
+        models.add(new Model(R.drawable.veterinaria, "Veterinaria"));
+        models.add(new Model(R.drawable.historia, "Historia clínica"));
+        models.add(new Model(R.drawable.notificaciones, "Notificaciones"));
+
+
+        adapter = new Adapter(models, this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+        Integer[] colors_temp = {
+                getResources().getColor(R.color.color1),
+                getResources().getColor(R.color.color2),
+                getResources().getColor(R.color.color3),
+
+        };
+
+        colors = colors_temp;
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset,
+                                    colors[position],
+                                    colors[position + 1]
+                            )
+                    );
+                }
+
+                else {
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+}
