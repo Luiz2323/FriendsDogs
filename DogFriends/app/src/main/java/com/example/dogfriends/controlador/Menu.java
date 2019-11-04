a279de7c041ff414901b85dbb907f04b8e8aaa2d
Luiz2323 <Lemunoz390@misena.edu.co>
2019-11-04 16:17:52 -0500

    Cambios en DogFriends

---
+++ DogFriends/app/src/main/java/com/example/dogfriends/controlador/Menu.java
@@ -0,0 +1,17 @@
+package com.example.dogfriends.controlador;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.dogfriends.R;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
