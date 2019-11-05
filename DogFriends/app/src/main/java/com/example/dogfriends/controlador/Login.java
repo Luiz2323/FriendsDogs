9b52d0c8beb8b1e43c094928668d6bfd134d3546
Luiz2323 <Lemunoz390@misena.edu.co>
2019-11-04 22:01:35 -0500

    Cambios en DogFriends

--- DogFriends/app/src/main/java/com/example/dogfriends/controlador/Login.java
+++ DogFriends/app/src/main/java/com/example/dogfriends/controlador/Login.java
@@ -3,14 +3,28 @@ package com.example.dogfriends.controlador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.dogfriends.R;
 
 public class Login extends AppCompatActivity {
+    private ImageView logo;
+    private Animation scale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inicializar();
+    }
+
+    private void inicializar() {
+        logo = findViewById(R.id.logo);
+        scale = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+        logo.startAnimation(scale);
+
+
     }
 }
