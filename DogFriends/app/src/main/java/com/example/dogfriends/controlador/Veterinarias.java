e5104949545241968626ea246d91ceb1bdeddcdd
Luiz2323 <Lemunoz390@misena.edu.co>
2019-11-11 12:26:14 -0500

    actualizacion

---
+++ DogFriends/app/src/main/java/com/example/dogfriends/controlador/Veterinarias.java
@@ -0,0 +1,16 @@
+package com.example.dogfriends.controlador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dogfriends.R;
+
+public class Veterinarias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veterinarias);
+    }
+}
